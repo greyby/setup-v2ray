63712368caa4f1664745565dcdd67d07c0a1a597
greyby <greyby.5@gmail.com>
2020-04-02 01:24:48 +0800

    fix privilege bug

--- setup_v2ray.sh
+++ setup_v2ray.sh
@@ -513,8 +513,8 @@ remove_acme() {
 
 open_bbr(){
 	colorEcho ${BLUE} "Preparing open bbr..."
-	sudo echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-	sudo echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	sudo bash -c "echo 'net.core.default_qdisc=fq' >> /etc/sysctl.conf"
+	sudo bash -c "echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf"
 	sudo sysctl -p
 }
 
